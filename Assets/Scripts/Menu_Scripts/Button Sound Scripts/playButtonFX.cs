a86232e0eb5512deb6a169db41819d3c6276fdf8
harisrocker1 <harisrocker1@gmail.com>
2019-11-11 09:53:03 -0500

    MAIN MENU _ Button Hovers added

---
+++ Assets/Scripts/Menu_Scripts/Button Sound Scripts/playButtonFX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playButtonFX : MonoBehaviour
+{
+    public AudioSource myFX;
+    public AudioClip hoverSound;
+    public AudioClip clickSound;
+
+    public void HoverSound()
+    {
+        myFX.PlayOneShot(hoverSound);
+    }
+    public void ClickSound()
+    {
+        myFX.PlayOneShot(clickSound);
+    }
+}
