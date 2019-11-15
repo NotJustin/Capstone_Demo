dca7048e2dd7b058fb04486e300e6d3eba350cea
harisrocker1 <harisrocker1@gmail.com>
2019-11-14 20:07:20 -0500

    Volume Slider Working for Main menu

---
+++ Assets/OptionMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionMenu : MonoBehaviour
+{
+
+    public AudioMixer audiomixer;
+
+
+    public void SetVolume (Slider slider)
+    {
+        audiomixer.SetFloat("volume",slider.value);
+        Debug.Log(slider.value);
+    }
+}
