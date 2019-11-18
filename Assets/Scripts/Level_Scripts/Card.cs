0ac45fc9ad80e046adae33737f2666304e4464db
Haris Rao <37711214+harisrao1@users.noreply.github.com>
2019-11-17 22:43:22 -0500

    mainmenuchit

--- Assets/Scripts/Level_Scripts/Card.cs
+++ Assets/Scripts/Level_Scripts/Card.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Card : MonoBehaviour
+public class card : MonoBehaviour
 {
     string attackType;
     int cost;
